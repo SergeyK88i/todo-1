9abf09adcf934597d427e95d3e7c7def6512b848
SergeyK88i <s19k88i@gmail.com>
2021-06-18 10:00:50 +0300

    выполнили подсветку в по категориям добавили курсор

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,20 +2,21 @@ import { Injectable } from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
 import {Task} from "../model/Task";
-import {Subject} from "rxjs";
+import {BehaviorSubject, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  taskSubject = new Subject<Task[]>();
+  taskSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories);
 
-  constructor() { }
-
-  getCategories():Category[] {
-    return TestData.categories;
+  constructor() {
+    this.filTask();
   }
+
+
   filTask(){
     this.taskSubject.next(TestData.tasks);
   }
--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -10,6 +10,7 @@ import {Category} from "../../model/Category";
 export class CategoriesComponent implements OnInit {
 
   categories: Category[] | undefined;
+  selectedCategory: Category | undefined;
 
 // когда загружается приложение ангуляр сам создает объекты классов(экземпляры)
 // для того что бы получить доступ(ссылку) на объекты создаем в конструкторе переменную с типом необходимого класса
@@ -20,12 +21,13 @@ export class CategoriesComponent implements OnInit {
 // метод вызывается автоматически после инициализации компонента
   ngOnInit(): void {
    // использованный метод положили в переменную, которую создали выше, там хранятся данные в виде массива
-    this.categories = this.dataHandler.getCategories();
-    console.log(this.categories);
+    this.dataHandler.categoriesSubject.subscribe(categories => this.categories = categories);
+
 
   }
 
   showByCategory(category: Category) {
+    this.selectedCategory = category;
     this.dataHandler.filTaskByCategory(category);
   }
 }
