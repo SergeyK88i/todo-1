61368a17eee5d3ade47585bc4bbd37ae4263fa1a
SergeyK88i <s19k88i@gmail.com>
2021-06-18 10:23:29 +0300

    добавили чекбоксы и цвета

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -17,4 +17,7 @@ export class TasksComponent implements OnInit {
     this.dataHandler.taskSubject.subscribe(tas =>this.tasks = tas);
   }
 
+  toggleTaskCompleted(task: Task) {
+    task.completed =!task.completed;
+  }
 }
